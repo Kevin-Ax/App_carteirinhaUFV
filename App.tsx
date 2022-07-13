bd34f97eb8ce0e4a84caa74003315b5443c9480b
Kevin <kevinlorecine@gmail.com>
2022-07-13 15:57:46 -0300

    Teste de sintax e limpeza de projeto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BotaoLogin from './assets/components/Login/botaoLogin';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BotaoLogin/>
     </View>
   );
 }
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ assets/components/Login/botaoLogin.tsx
@@ -0,0 +1,10 @@
+import { Button, Text } from "react-native";
+
+function BotaoLogin(){
+    return (
+        <Button title="Login" />
+    );
+}
+
+
+export default BotaoLogin;
