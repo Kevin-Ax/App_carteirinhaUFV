e895376e5e6c25821f80c09851841b446d30ccb1
Kevin <kevinlorecine@gmail.com>
2022-07-18 13:48:07 -0300

    Mudança no Route

--- Routes.tsx
+++ Routes.tsx
@@ -2,16 +2,17 @@ import { Router, Scene } from 'react-native-router-flux'
 import TelaLogin from './assets/components/Login/TelaLogin';
 import TelaCadastro from './assets/components/cadastro/TelaCadastro';
 import TelaMain from './assets/components/Main/TelaMain';
+import { View } from 'react-native';
 
 function Routes(){
     return(
-        <Router>
-            <Scene key='root'>
-            <Scene key = "TelaLogin" component = {TelaLogin} title = "TelaLogin" initial = {true} />
-            <Scene key = "Cadastro" component = {TelaCadastro} title = "Cadastro" />
-            <Scene key = "TelaPrincipal" component = {TelaMain} title = "TelaPrincipal" />
-            </Scene>
-        </Router>
+        <View>
+            <Router>
+                <Scene key='root'>
+                    <Scene key = "TelaLogin" component = {TelaLogin} title = "TelaLogin" initial = {true} />
+                </Scene>
+            </Router>
+        </View>
     );
 }
 
