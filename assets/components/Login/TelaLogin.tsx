0593bcc6dad019181324f3e18d471407ec88da98
Paulo Dezingrini <paulo.dezingrini@ufv.br>
2022-07-30 19:13:57 -0300

    Formatações

--- assets/components/Login/TelaLogin.tsx
+++ assets/components/Login/TelaLogin.tsx
@@ -1,144 +1,156 @@
-import { Pressable, TextInput, View, StyleSheet, Text, Image, Button } from 'react-native';
-import React from 'react';
+import { Pressable, TextInput, View, StyleSheet, Text, Image, Button, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import firebase from '../../config/firebase';
 
-function TelaLogin(){
-    return(
-        <View style={style.container}>
+function TelaLogin() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorLogin, setErrorLogin] = useState("")
+
+    const loginFirebase = () => { }
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={style.container}>
             <Image
-                source={{uri:'https://5287aa00874a313e299d-1850966fc307ff23e1e789aeafd2476b.ssl.cf5.rackcdn.com/PostImagem/42240/foto-branca-download-wallpapers-e-onde-usar_o1fcm9aiph1mte1km1f11cs53mng.png'}}
+                source={{ uri: 'https://5287aa00874a313e299d-1850966fc307ff23e1e789aeafd2476b.ssl.cf5.rackcdn.com/PostImagem/42240/foto-branca-download-wallpapers-e-onde-usar_o1fcm9aiph1mte1km1f11cs53mng.png' }}
                 style={style.background}
             >
             </Image>
             <Image
-                source={{uri:'https://dci.ufv.br/wp-content/uploads/2018/02/800x600-sem_inscricao.png'}}
+                source={{ uri: 'https://dci.ufv.br/wp-content/uploads/2018/02/800x600-sem_inscricao.png' }}
                 style={style.logo}
-                >
+            >
             </Image>
             <Text
                 style={style.Title}
-                >Carteirinha Virtual
+            >Carteirinha Virtual
             </Text>
             <Text
                 style={style.TextMatricula}
-                >Matricula
+            >Matricula
             </Text>
             <TextInput
-                style = {style.inputText}
+                style={style.inputText}
                 placeholder='Matricula'
             />
             <Text
                 style={style.TextSenha}
-                >Senha
+            >Senha
             </Text>
             <TextInput
-                style = {style.inputText}
+                style={style.inputText}
                 secureTextEntry={true}
                 placeholder='Senha'
             />
             <Pressable style={style.button}>
                 <Text
                     style={style.TextButton}
-                    >Login
+                >Login
                 </Text>
             </Pressable>
             <Text style={style.Text2}
-                >Não tem conta ainda? 
-                    <Pressable
+            >Não tem conta ainda?
+                <Pressable
                     //onPress={ (navigation) => navigation.navigate('./assets/components/Login/TelaLogin')}
-                        style={style.button2}
-                        >
-                        <Text
-                            style={style.TextButton2}
-                            >Fazer /Cadastro
-                        </Text>
-                    </Pressable>
+                    style={style.button2}
+                >
+                    <Text
+                        style={style.TextButton2}
+                    >Fazer /Cadastro
+                    </Text>
+                </Pressable>
             </Text>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
 
 
 //Css da página
 const style = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        justifyContent:'center',
-        backgroundColor:'#e6be8a'
+        justifyContent: 'center',
+        backgroundColor: '#e6be8a'
     },
-    logo:{
-        alignSelf:'center',
-        width:'65%',
-        height:'15%',
-        marginTop:-230,
-        marginBottom:150,
+    logo: {
+        alignSelf: 'center',
+        width: '65%',
+        height: '15%',
+        marginTop: -230,
+        marginBottom: 150,
     },
-    background:{
-        alignSelf:'center',
-        width:'100%',
-        height:'81%',
-        marginTop:-460,
-        borderRadius:80,
-        borderr:10
+    background: {
+        alignSelf: 'center',
+        width: '100%',
+        height: '81%',
+        marginTop: -460,
+        borderRadius: 80,
+        borderr: 10
     },
-    inputText:{
-        alignSelf:'center',
-        justifyContent:'center',
-        width:'60%',
-        height:'4%',
-        backgroundColor:'white',
-        marginVertical:'4%',
-        borderRadius:60,
-        marginBottom:12
+    inputText: {
+        alignSelf: 'center',
+        justifyContent: 'center',
+        width: '60%',
+        height: '4%',
+        backgroundColor: 'white',
+        marginVertical: '4%',
+        borderRadius: 60,
+        marginBottom: 12,
+        paddingLeft: 10
     },
-    Title:{
-        alignSelf:'center',
-        color:'black',
-        fontSize:28,
-        marginTop:-130
+    Title: {
+        alignSelf: 'center',
+        color: 'black',
+        fontSize: 28,
+        marginTop: -130
     },
-    TextMatricula:{
-        alignSelf:'center',
-        color:'white',
-        fontSize:22,
-        marginTop:15,
-        paddingTop:50,
+    TextMatricula: {
+        alignSelf: 'center',
+        color: 'white',
+        fontSize: 22,
+        marginTop: 15,
+        paddingTop: 50,
     },
-    TextSenha:{
-        alignSelf:'center',
-        color:'white',
-        fontSize:22,
+    TextSenha: {
+        alignSelf: 'center',
+        color: 'white',
+        fontSize: 22,
     },
-    Text2:{
-        alignSelf:'center',
-        color:'black',
-        fontSize:14,
+    Text2: {
+        alignSelf: 'center',
+        color: 'black',
+        fontSize: 14,
     },
-    TextButton:{
-        alignSelf:'center',
-        color:'white',
-        fontSize:22
+    TextButton: {
+        alignSelf: 'center',
+        color: 'white',
+        fontSize: 22
     },
-    TextButton2:{
-        alignSelf:'center',
-        color:'blue',
-        fontSize:12,
-        textDecorationLine:'underline',
-        marginTop:16
+    TextButton2: {
+        alignSelf: 'center',
+        color: 'blue',
+        fontSize: 12,
+        textDecorationLine: 'underline',
+        marginTop: 16
     },
-    button:{
-        alignSelf:'center',
-        width:'60%',
-        height:'4.88%',
-        backgroundColor:'#9b111e',
-        color:'white',
-        borderRadius:60,
-        marginTop:15,
-        marginBottom:20
+    button: {
+        alignSelf: 'center',
+        width: '60%',
+        height: '4.88%',
+        backgroundColor: '#9b111e',
+        color: 'white',
+        borderRadius: 60,
+        marginTop: 15,
+        marginBottom: 20
     },
-    button2:{
-        width:'60%',
-        height:'4.88%',
-        marginTop:8
+    button2: {
+        width: '60%',
+        height: '4.88%',
+        marginTop: 8
     }
 })
 
